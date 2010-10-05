bbb27dd089d5fe213ce5590b21fedbc917b02a21
Norichika.Murayama <mura869@gmail.com>
2010-10-05 21:35:14 +0900

    ロードパスの設定の仕方を変えてみた

--- imgcvt.rb
+++ imgcvt.rb
@@ -4,7 +4,8 @@ require 'rubygems'
 require 'RMagick'
 
 module Imgcvt
-  $LOAD_PATH << 'lib/Imgcvt'
+#  $LOAD_PATH << 'lib/Imgcvt'
+  $:.unshift('lib/Imgcvt')
   autoload :Main, 'imgcvt/main'
   autoload :Options, 'imgcvt/options'
   autoload :Converter, 'imgcvt/converter'
